a97553d73c21d146d70f78062c8af9c78ad39b6b
SebLOURDEL <seb.lourdel8@gmail.com>
2024-09-18 14:48:51 +0200

    create nuxtapp

---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">À propos</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav ul {
+  list-style: none;
+  display: flex;
+  gap: 15px;
+}
+nav a {
+  text-decoration: none;
+  color: #2c3e50;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Navbar />
+    <Hero />
+    <h1>Bienvenue sur la nouvelle homepage</h1>
+    <p>Ceci est la réplique de la homepage que vous souhaitez recréer.</p>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '~/components/Navbar.vue'
+import Hero from '~/components/Hero.vue'
+</script>
