7211d99f96b410a8f233e75a5a84992470d07118
SebLOURDEL <seb.lourdel8@gmail.com>
2024-09-20 15:56:59 +0200

    change package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   plugins: [
     '~/plugins/locomotiveScroll.client.js',
     '~/plugins/gsap.client.js'
 
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
-  }
+  },
+
+  compatibilityDate: '2024-09-20'
 });
